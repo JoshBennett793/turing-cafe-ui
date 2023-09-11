c69e338df13a777acb8d767acdc4e5ca7481d166
Josh Bennett <joshbennett793@yahoo.com>
2023-09-11 12:52:19 -0500

    Light testing of initial page load contents

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,32 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {
+      statusCode: 200,
+      fixture: 'reservations.json'
+    }).as('fetchCall');
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Should render the home page with header, form, and cards', () => {
+    cy.wait('@fetchCall');
+    cy.contains('Turing Cafe Reservations');
+
+    cy.get('.res-form').children().should('have.length', 5);
+
+    cy.get('.resy-container').children().should('have.length', 3);
+
+    cy.get('.resy-container')
+      .children()
+      .first()
+      .children('ul')
+      .children()
+      .should('have.length', 4);
+
+    cy.get('.resy-container')
+      .children()
+      .first()
+      .children('ul')
+      .children()
+      .should('have.length', 4);
+  });
+});
