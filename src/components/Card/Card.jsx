9271e28420fea1cf543410b999cc383c3e4d6dd1
Josh Bennett <joshbennett793@yahoo.com>
2023-09-11 10:59:47 -0500

    Create a single card with reservation data

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+export default function Card({ reservation }) {
+  const { name, date, time, number } = reservation;
+
+  return (
+    <div className='res-card'>
+      <ul>
+        <li>{name}</li>
+        <li>{date}</li>
+        <li>{time}</li>
+        <li>{number}</li>
+      </ul>
+    </div>
+  );
+}
