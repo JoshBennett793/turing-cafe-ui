fc5c378502ab034504e3f241e8e57487caceff8a
Josh Bennett <joshbennett793@yahoo.com>
2023-09-11 11:56:54 -0500

    Control form data

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,12 +8,50 @@ export default function Form() {
     number: 0
   });
 
+  const handleChange = e => {
+    const { name, value, type } = e.target;
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: type === 'number' ? parseInt(value) : value
+      };
+    });
+  };
+
   return (
     <form>
-      <input type='text' name='name' id='name' placeholder='Name' />
-      <input type='text' name='date' id='date' placeholder='Date (mm/dd)' />
-      <input type='text' name='time' id='time' placeholder='Time' />
-      <input type='number' name='number' id='number' placeholder='Number of guests' />
+      <input
+        type='text'
+        name='name'
+        id='name'
+        placeholder='Name'
+        onChange={handleChange}
+        value={formData.name}
+      />
+      <input
+        type='text'
+        name='date'
+        id='date'
+        placeholder='Date (mm/dd)'
+        onChange={handleChange}
+        value={formData.date}
+      />
+      <input
+        type='text'
+        name='time'
+        id='time'
+        placeholder='Time'
+        onChange={handleChange}
+        value={formData.time}
+      />
+      <input
+        type='number'
+        name='number'
+        id='number'
+        placeholder='Number of guests'
+        onChange={handleChange}
+        value={formData.number}
+      />
     </form>
   );
 }
